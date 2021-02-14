d016201a7aff79484851488a2dab1bd488f4fe8e
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2021-02-14 02:04:41 +0100

    #7 sprites sheet based you win screen

---
+++ youwin.js
@@ -0,0 +1,35 @@
+let soundtrack;
+let filmReelLoop;
+let youWinAnim;
+
+function preload() {
+    soundtrack = loadSound('assets/SpiegelImSpiegelMonoShort.mp3');
+    filmReelLoop = loadSound('assets/mixkit-explainer-video-foley-projector-hybrid-film-antique-fade-pitch-bend-3012.wav');
+    youWinAnim = loadAnimation('sprites/youwin_variants/youwin-01.png', 'sprites/youwin_variants/youwin-08.png');
+}
+
+function setup() {
+    setupCanvas(false);
+    cursor(HAND);
+    soundtrack.loop();
+    filmReelLoop.loop();
+}
+
+function draw() {
+    // Chrome hack
+    if (!soundtrack.isPlaying()) {
+        soundtrack.loop();
+    }
+    if (!filmReelLoop.isPlaying()) {
+        filmReelLoop.loop();
+    }
+    background(255);
+    animation(youWinAnim, width / 2, height / 2);
+}
+
+function mouseReleased() {
+    userStartAudio(); // Chrome hack
+    if (mouseX < width && mouseX > 0 && mouseY < height && mouseY > 0) {
+        redirectToUrlFor('makingof');
+    }
+}
