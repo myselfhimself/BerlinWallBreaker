9321549b7e6c1537593c01b1dbace6efdfcc9d06
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2021-02-24 23:52:38 +0100

    early level+lives detection from anchor substring

--- common.js
+++ common.js
@@ -33,21 +33,42 @@ var BWB_GAME_STATE_PLAYING = 1;
 var BWB_GAME_STATE_WIN = 2;
 var BWB_GAME_STATE_PAUSED = 3;
 var BWB_GAME_STATE = BWB_GAME_STATE_PLAYING;
+var BWB_DEFAULT_GAME_LIVES = 3;
+var BWB_GAME_LIVES = BWB_DEFAULT_GAME_LIVES;
+var BWB_DEFAULT_LEVEL_ID = 0;
 
-// LEVEL DETECTION AND REDIRECTION FROM URL
+// LEVEL AND LIVES COUNT DETECTION AND REDIRECTION FROM URL
+// anchor must be like: #levelNlivesM (if provided), defaults to #level0lives3 (ie. level 1, 3 lives)
 var BWB_REDIRECTING = false;
-var BWB_LEVEL_ID = location.hash;
+var BWB_LEVEL_KEYWORD = 'level'; //  levelN: current (or target level)
+var BWB_LIVES_KEYWORD = 'lives'; // livesN: remaining lives
+var BWB_LEVEL_ID = BWB_DEFAULT_LEVEL_ID; // Integer
+var BWB_LEVEL_URL_DATA = location.hash; // Default BWB_LEVEL_ID is full hash. Will be just an integer after then.
+
+if (BWB_LEVEL_URL_DATA == undefined || BWB_LEVEL_URL_DATA.indexOf('#' + BWB_LEVEL_KEYWORD) != 0 || BWB_LEVEL_URL_DATA.length < ('#' + BWB_LEVEL_KEYWORD).length+1) {
+    console.log('Level is unset, defaulting to 1');
+    BWB_LEVEL_ID = 0;
+} else {
+    BWB_LEVEL_ID = parseInt(BWB_LEVEL_URL_DATA.substr(('#' + BWB_LEVEL_KEYWORD).length, 1));
+    console.log('Level is set: ' + BWB_LEVEL_ID.toString());
+}
+if (BWB_LEVEL_URL_DATA == undefined || BWB_LEVEL_URL_DATA.indexOf(BWB_LIVES_KEYWORD) == -1) {
+    //BWB_GAME_LIVES = BWB_GAME_LIVES;
+    console.log('Lives is unset, defaulting to ' + BWB_GAME_LIVES.toString());
+} else {
+    BWB_GAME_LIVES = parseInt(BWB_LEVEL_URL_DATA.substr(('#' + BWB_LEVEL_KEYWORD + 'N' + BWB_LIVES_KEYWORD).length, 1));
+    console.log('Lives is set: ' + BWB_GAME_LIVES.toString());
+}
+// END OF LEVEL AND LIVES COUNT DETECTION
+
 var BWB_URL_GAME_OVER = 'level_intro.html#level3';
 var BWB_URL_WIN = 'youwin.html';
 var BWB_URL_HOME = 'home.html';
 var BWB_URL_MAKING_OF = 'makingof.html';
 
-if (BWB_LEVEL_ID == undefined || BWB_LEVEL_ID.indexOf('#level') != 0 || BWB_LEVEL_ID.length != '#level1'.length) {
-    console.log('Level is unset, defaulting to 1');
-    BWB_LEVEL_ID = 0;
-} else {
-    BWB_LEVEL_ID = parseInt(BWB_LEVEL_ID.substr('#level'.length, 1));
-}
+
+// TODO GAME OVER PARAMETERS PASSING (level + lives)
+// TODO GAME OVER TO TRY AGAIN TEXT AS LONG AS LIVES ARE NOT FINISHED
 
 function urlFor(slug) {
     if (slug.indexOf('level') == 0) {
