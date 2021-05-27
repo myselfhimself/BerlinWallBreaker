981d49ef133ab00439b5a9cd38bdf5ccbb9b1b25
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2021-05-27 18:50:43 +0200

    making sprites foreground extracting tools accept dimensions

--- sprites/food/level1/gmic_extract_foregrounds.bash
+++ sprites/food/level1/gmic_extract_foregrounds.bash
@@ -3,8 +3,10 @@ currentTime=$(date +%H%M)
 mkdir $currentTime
 echo Prefix of jpg files? 
 read jpgPrefix
+echo "Dimensions (comma separated like 100,100)?"
+read dimensions
 for A in $jpgPrefix*.jpg; do 
 	simpleFileName=$(basename -s .jpg $A)
 	echo simpleFileName is $simpleFileName
-	gmic $A fx_extract_foreground 0,0,3,1,2560,1920,1104.31372,1952.62744,1049.09802,439.215698,0,0,2,1 rm[1] display output $currentTime/$simpleFileName.png resize 30,30 output $currentTime/${simpleFileName}_small.png;
+	gmic $A fx_extract_foreground 0,0,3,1,2560,1920,1104.31372,1952.62744,1049.09802,439.215698,0,0,2,1 rm[1] display output $currentTime/big_$simpleFileName.png resize $dimensions output $currentTime/${simpleFileName}.png;
 done
